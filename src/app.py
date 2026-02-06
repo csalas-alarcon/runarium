f502c62d6900f2fafad0602991682e7602d23f21
Csalas0821 <csalas0821@proton.me>
2026-02-06 12:17:49 +0100

    Mastering Textual

--- src/app.py
+++ src/app.py
@@ -1,20 +1,42 @@
+from time import monotonic
 
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header, Button, Digits
 from textual.containers import HorizontalGroup, VerticalScroll
+from textual.reactive import reactive
 
 class TimeDisplay(Digits):
-    ...
+    start_time = reactive(monotonic)
+    time = reactive(0.0)
+    total = reactive(0.0)
+
+    def on_mount(self) -> None:
+        self.update_timer = self.set_interval(1 / 60, self.update_time, pause=True)
+
+    def update_time(self) -> None:
+        self.time = self.total + (monotonic() - self.start_time)
+
+    def watch_time(self, time: float) -> None:
+        minutes, seconds = divmod(time, 60)
+        hours, minutes = divmod(minutes, 60)
+        self.update(f"{hours:02,.0f}:{minutes:02.0f}:{seconds:05.2f}")
 
 class Stopwatch(HorizontalGroup):
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "start":
+            self.add_class("started")
+        elif event.button.id == "stop":
+            self.remove_class("started")
 
     def compose(self) -> ComposeResult:
         yield Button("Start", id="start", variant="success")
         yield Button("Stop", id="stop", variant="error")
         yield Button("Reset", id="reset")
-        yield TimeDisplay("00:00:00:00")
+        yield TimeDisplay()
 
 class StopwatchApp(App):
+    CSS_PATH = "stopwatch03.tcss"
     BINDINGS = [("d", "toogle_dark", "Toogle dark mode")]
 
     def compose(self) -> ComposeResult:
